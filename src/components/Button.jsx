68e7d85b57754f82bcdf44a0eb10f5d393af958f
Sapphire Smith <ssapphire527@gmail.com>
2024-07-16 12:05:11 +0530

    Special offers section completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,13 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor, }) => {
     return (
-        <button className="flex justify-center
+        <button className={`flex justify-center
          items-center gap-2 px-7 py-4 border font-montserrat text-lg 
-         leading-none bg-coral-red rounded-full text-white border-coral-red">
+         leading-none
+         ${backgroundColor
+                ? `${backgroundColor} ${textColor} ${borderColor}`
+
+                : "bg-coral-red text-white border-coral-red"}
+          rounded-full`}>
             {label}
             {iconURL &&
                 <img src={iconURL}
