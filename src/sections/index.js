a9d318580c5dc5e6655cd37c8afb4adab97220e4
Sapphire Smith <ssapphire527@gmail.com>
2024-07-16 11:08:42 +0530

    quality section completed

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+
+export {
+    Hero,
+    PopularProducts,
+    Subscribe,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Footer
+}
